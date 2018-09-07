2186bc7993005e9005f203668182d7ae284dc730
Mario Rodriguez <mario.rodriguez35@yahoo.com>
2018-09-07 01:41:51 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -10,5 +10,9 @@ function forLoop(array) {
 }
 
 function whileLoop(n) {
-    while (countdown > )
+    let countdown = n
+    while (countdown > 0) {
+      console.log(--countdown)
+    }
+    return 'done'
 }
